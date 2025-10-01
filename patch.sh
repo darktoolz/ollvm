45ded15c1a9c632d99901963bbab66321f77f038
darktoolz <soma@somahorse.com>
2025-10-02 02:58:30 +0300

    add Dockerfile patch

--- patch.sh
+++ patch.sh
@@ -3,6 +3,7 @@
 set -x
 
 git clone --depth 1 --branch 20231128 https://github.com/mstorsjo/llvm-mingw.git && \
-  cp Function.h.patch Utils.cpp.patch llvm-mingw/ && \
+  cp *.patch llvm-mingw/ && \
   cd llvm-mingw && \
-  patch -p1 < ../build-llvm.sh.patch
+  patch -p1 < build-llvm.sh.patch && \
+  patch -p1 < Dockerfile.patch
