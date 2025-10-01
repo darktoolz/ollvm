1de6a89f7ad6d9475d49bb97df7da7780f94ce92
darktoolz <soma@somahorse.com>
2025-10-02 02:28:14 +0300

    verbose

--- patch.sh
+++ patch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -x
+
 git clone --depth 1 --branch 20231128 https://github.com/mstorsjo/llvm-mingw.git && \
-  cp Function.h.patch Utils.cpp.patch llvm-mingw && \
+  cp Function.h.patch Utils.cpp.patch llvm-mingw/ && \
   cd llvm-mingw && \
   patch -p1 < ../build-llvm.sh.patch
