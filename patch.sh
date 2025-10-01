34365bc9b32d92c7561624d6017ec887aad7e739
darktoolz <soma@somahorse.com>
2025-10-02 02:14:57 +0300

    fix patches

--- patch.sh
+++ patch.sh
@@ -2,4 +2,5 @@
 
 git clone --depth 1 --branch 20231128 https://github.com/mstorsjo/llvm-mingw.git && \
   cd llvm-mingw && \
-  patch -p1 < ../build-llvm.sh.patch
+  patch -p1 < ../build-llvm.sh.patch && \
+  cp Function.h.patch Utils.cpp.patch llvm-mingw
