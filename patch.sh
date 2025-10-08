b93104b2d7f65fcbb37c0c7b09a73d6779fa3bcc
darktoolz <soma@somahorse.com>
2025-10-08 03:39:26 +0300

    to LLVM_ENABLE_LLD OFF in CMakeLists

--- patch.sh
+++ patch.sh
@@ -6,5 +6,6 @@ git clone --depth 1 --branch 20240619 https://github.com/mstorsjo/llvm-mingw && 
   git clone -b main https://github.com/sr-tream/obfuscator && \
   cp *.patch obfuscator/obfuscator.patch llvm-mingw && \
   cd llvm-mingw && \
+  patch -p1 < obfuscator-CMakeLists.rev.patch && \
   patch -p1 < Dockerfile.patch && \
   patch -p1 < build-llvm.sh.patch
